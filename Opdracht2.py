d366cb7b7815ec2defeb062ff3a53735b353985a
Atilla-1101070 <you@example.com>
2024-09-04 13:15:04 +0200

    Finished the second part of Opdracht2

--- Opdracht2.py
+++ Opdracht2.py
@@ -6,4 +6,10 @@ y = math.sqrt(3 * x - 1) + pow((1 + x),2)
 
 print(y)
 #Berekening2
+a = 0.87
+b = 22.7
+c = 5.03
+
+y = (-b + math.sqrt(pow(b,2)-4*a*c))/(2*a)
+print(f"De waarde van y = {y} als a = {a}, b = {b} en c = {c}")
 
