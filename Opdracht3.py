0f5c837e1478b813736affa886124a70a859cbc0
Atilla-1101070 <you@example.com>
2024-09-04 13:51:28 +0200

    Finished the first part of Opdracht3

---
+++ Opdracht3.py
@@ -0,0 +1,15 @@
+#InputVragen
+connectionType = input("Welke verbinding wilt U gebruiken [4G, 5G, Wifi open]: ")
+
+if connectionType == "4G":
+    print(f"U bent verbonden via {connectionType}.")
+elif connectionType == "5G":
+    print(f"U bent verbonden via {connectionType}.")
+elif connectionType == "Wifi open":
+    warn = input("U heeft de voor de Wifi open gekozen, wij wijzen u erop dat de data door de eigenaar van dit netwerk is te lezen.")
+    if warn == "ja":
+        print(f"U bent verbonden via {connectionType}.")
+    else:
+        print("U bent niet verbonden!")
+else:
+    print("Onbekende invoer, er wordt geen verbinding tot stand gebracht.")
