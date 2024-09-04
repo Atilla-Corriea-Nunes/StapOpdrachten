8ed5f283d2871ddb12896d7c54ed7c1206eac8b7
Atilla-1101070 <you@example.com>
2024-09-04 13:24:16 +0200

    Finished the third and ast part of Opdracht2

--- Opdracht2.py
+++ Opdracht2.py
@@ -10,6 +10,13 @@ a = 0.87
 b = 22.7
 c = 5.03
 
-y = (-b + math.sqrt(pow(b,2)-4*a*c))/(2*a)
+y = (-b+math.sqrt(pow(b,2)-4*a*c))/(2*a)
 print(f"De waarde van y = {y} als a = {a}, b = {b} en c = {c}")
+#Berekening3
+t = 4
+v = 179875474.8
+c = 299792458
 
+delta = t*1/(1/(v*(1-(pow(v,2))/(pow(c,2)))))
+
+print(f"Vanaf een komeet gezien zit u {delta} uur op de tuinstoel")
